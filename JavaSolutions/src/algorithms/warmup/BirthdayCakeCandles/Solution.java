8a7839e26168d3da7e433def534a41c520497e37
dtcline <dtclin3@gmail.com>
2017-08-05 13:47:25 -0400

    Implemented solution

--- JavaSolutions/src/algorithms/warmup/BirthdayCakeCandles/Solution.java
+++ JavaSolutions/src/algorithms/warmup/BirthdayCakeCandles/Solution.java
@@ -1,15 +1,21 @@
 package algorithms.warmup.BirthdayCakeCandles;
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
     static int birthdayCakeCandles(int n, int[] ar) {
-        // Complete this function
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+        for (int i : ar) {
+            if (i > max) { // found new max so set new max and start new count
+                max = i;
+                count = 1;
+            } else if (i == max) {
+                ++count;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {

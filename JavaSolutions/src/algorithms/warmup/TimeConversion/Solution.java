19e7505e3ae1b60fdb27c037c503b48ead2d8d87
dtcline <dtclin3@gmail.com>
2017-08-05 14:02:46 -0400

    Added skeleton code

---
+++ JavaSolutions/src/algorithms/warmup/TimeConversion/Solution.java
@@ -0,0 +1,17 @@
+package algorithms.warmup.TimeConversion;
+
+import java.util.*;
+
+public class Solution {
+
+    static String timeConversion(String s) {
+        // Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String result = timeConversion(s);
+        System.out.println(result);
+    }
+}

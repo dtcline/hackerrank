94d16f1a6fd1998c86584ed72a3d4740d76de71e
dtcline <dtclin3@gmail.com>
2017-08-05 13:22:59 -0400

    Implemented solution

--- JavaSolutions/src/algorithms/warmup/Staircase/Solution.java
+++ JavaSolutions/src/algorithms/warmup/Staircase/Solution.java
@@ -1,11 +1,31 @@
 package algorithms.warmup.Staircase;
 
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
 
+    private static void appendNStrings(StringBuilder sb, int n, String c) {
+        for (int i = 0; i < n; ++i) {
+            sb.append(c);
+        }
+    }
+
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        String space = " ";
+        String octothorpe = "#";
+        StringBuilder result = new StringBuilder();
+        int spaces = n - 1;
+        int octothorpes = 1;
+        for (int i = 0; i < n; ++i) {
+            appendNStrings(result, spaces, space);
+            appendNStrings(result, octothorpes, octothorpe);
+            result.append("\n");
+            --spaces;
+            ++octothorpes;
+        }
+        System.out.println(result);
     }
 }

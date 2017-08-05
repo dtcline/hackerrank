06365c66d385168edef5e36504d3039f2a676a9e
dtcline <dtclin3@gmail.com>
2017-08-05 13:01:20 -0400

    Implemented solution

--- JavaSolutions/src/algorithms/warmup/PlusMinus/Solution.java
+++ JavaSolutions/src/algorithms/warmup/PlusMinus/Solution.java
@@ -1,11 +1,48 @@
 package algorithms.warmup.PlusMinus;
 
-import java.io.*;
-import java.util.*;
+/*
+6
+-4 3 -9 0 4 1
+
+ */
+
+import java.util.Scanner;
 
 public class Solution {
 
+    private static void printResult(int count, int size) {
+        // special case: don't get tricked into dividing by 0 or dividing 0 by something else!
+        if (size == 0 || count == 0) {
+            System.out.println(0.0);
+        } else {
+            System.out.println(((double) count) / ((double) size));
+        }
+    }
+
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        // assume size is positive
+        int[] nums = new int[size];
+        for (int i = 0; i < size; ++i) {
+            nums[i] = in.nextInt();
+        }
+
+        int positives = 0;
+        int negatives = 0;
+        int zeroes = 0;
+        for (int i : nums) {
+            if (i > 0) {
+                ++positives;
+            } else if (i < 0) {
+                ++negatives;
+            } else {
+                ++zeroes;
+            }
+        }
+
+        printResult(positives, size);
+        printResult(negatives, size);
+        printResult(zeroes, size);
     }
 }
